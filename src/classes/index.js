d82294fdcc37b6e81678eb539cf591479377479d
luisedr98 <luisdominguez818@gmail.com>
2022-09-17 17:52:51 -0500

    agregando las clases TODO y TODOList

---
+++ src/classes/index.js
@@ -0,0 +1,7 @@
+import { Todo } from "./todo.class";
+import { TodoList } from "./TodoList.class";
+
+export {
+    Todo,
+    TodoList
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-import { saludar } from './js/componentes.mjs';
+import { Todo, TodoList } from './classes';
 import './style.css';
-const nombre = 'Eduardo'
-saludar(nombre)
+
+const todoList = new TodoList();
+
+const tarea = new Todo('Aprender JS');
+const tarea2 = new Todo('Esudiar Ingles');
+
+todoList.nuevoTodo(tarea),
+todoList.nuevoTodo(tarea2);
+
+console.log(todoList);
